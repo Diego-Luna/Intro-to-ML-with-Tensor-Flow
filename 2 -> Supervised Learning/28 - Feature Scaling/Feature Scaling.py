a1b97e68c74ff01c9794715314f47638522004be
Dieg Luna <diegofcolunalopez@gmail.com>
2021-12-28 11:39:14 -0600

    Feature Scaling 2/2

---
+++ 2 -> Supervised Learning/28 - Feature Scaling/Feature Scaling.py
@@ -0,0 +1,45 @@
+# TODO: Add import statements
+## Diego -> step 1
+import numpy as np
+import pandas as pd
+from sklearn.linear_model import Lasso
+from sklearn.preprocessing import StandardScaler
+
+# Assign the data to predictor and outcome variables
+# TODO: Load the data
+#train_data = None
+#X = None
+#y = None
+
+## Diego -> step 2
+train_data = pd.read_csv('data.csv', header = None)
+X = train_data.iloc[:,:-1]
+y = train_data.iloc[:,-1]
+
+# TODO: Create the standardization scaling object.
+#scaler = None
+## Diego -> step 3
+scaler = StandardScaler()
+
+# TODO: Fit the standardization parameters and scale the data.
+#X_scaled = None
+## Diego -> step 4
+X_scaled = scaler.fit_transform(X)
+
+# TODO: Create the linear regression model with lasso regularization.
+#lasso_reg = None
+## Diego -> step 5
+lasso_reg = Lasso()
+
+
+# TODO: Fit the model.
+## Diego -> step 6
+lasso_reg.fit(X_scaled, y)
+
+
+# TODO: Retrieve and print out the coefficients from the regression model.
+#reg_coef = None
+#print(reg_coef)
+## Diego -> step 7
+reg_coef = lasso_reg.coef_
+print(reg_coef)
